d622c455f19d456bace3ac6498596e333c3dd470
TylerMair <tyleramair@gmail.com>
2022-11-29 11:58:57 -0700

    HTML template files & Psuedo Code

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+
+#Create Flask instance
+app = Flask(__name__)
+
+db = SQLAlchemy(app)
+
+#Sign up (Add new user)
+
+#Teacher Login
+
+#Admin Login
+
+#Edit Teacher
+
+#Delete Teacher (Admin only)
+
+#Add Student
+
+#Update Student
+
+#Delete Student
+
+#Get School Calender
+
+#Get Your students for homepage
+
+#Single Student Page
+
+
+
+
